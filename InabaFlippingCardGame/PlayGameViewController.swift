a1493d666bfad8c71696b9085c438791e56658af
Takamasa Fukase <takama224@yahoo.co.jp>
2020-03-08 00:00:40 +0900

    タイトル画面を作成。

---
+++ InabaFlippingCardGame/PlayGameViewController.swift
@@ -0,0 +1,30 @@
+//
+//  PlayGameViewController.swift
+//  InabaFlippingCardGame
+//
+//  Created by 深瀬貴将 on 2020/03/07.
+//  Copyright © 2020 fukase. All rights reserved.
+//
+
+import UIKit
+
+class PlayGameViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
