d5b655d517dd6a847ed2def0a25219f3dffe5e12
Takamasa Fukase <takama224@yahoo.co.jp>
2020-03-08 10:59:31 +0900

    カードの並び方の変更。カード裏面の画像を表示。

--- InabaFlippingCardGame/PlayGameViewController.swift
+++ InabaFlippingCardGame/PlayGameViewController.swift
@@ -31,21 +31,26 @@ extension PlayGameViewController: UICollectionViewDelegate, UICollectionViewData
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = CollectionViewUtil.createCell(collectionView, identifier: CardCell.reusableIdentifier, indexPath) as! CardCell
-        switch indexPath.row {
-        case 0...4:
+//        switch indexPath.row {
+//        case 0...4:
+//            cell.imageView.image = UIImage(named: "CardBackImageRed")
+//        case 5...9:
+//            cell.imageView.image = UIImage(named: "CardBackImageBlue")
+//        case 10...14:
+//            cell.imageView.image = UIImage(named: "CardBackImageRed")
+//        case 15...19:
+//            cell.imageView.image = UIImage(named: "CardBackImageBlue")
+//        case 20...24:
+//            cell.imageView.image = UIImage(named: "CardBackImageRed")
+//        case 25...29:
+//            cell.imageView.image = UIImage(named: "CardBackImageBlue")
+//        default:
+//            break
+//        }
+        if indexPath.row % 2 == 0 {
             cell.imageView.image = UIImage(named: "CardBackImageRed")
-        case 5...9:
+        }else {
             cell.imageView.image = UIImage(named: "CardBackImageBlue")
-        case 10...14:
-            cell.imageView.image = UIImage(named: "CardBackImageRed")
-        case 15...19:
-            cell.imageView.image = UIImage(named: "CardBackImageBlue")
-        case 20...24:
-            cell.imageView.image = UIImage(named: "CardBackImageRed")
-        case 25...29:
-            cell.imageView.image = UIImage(named: "CardBackImageBlue")
-        default:
-            break
         }
         return cell
     }
@@ -54,13 +59,13 @@ extension PlayGameViewController: UICollectionViewDelegate, UICollectionViewData
 extension PlayGameViewController: UICollectionViewDelegateFlowLayout {
     //セクションの外側余白
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
-        return UIEdgeInsets(top: 2, left: 2, bottom: 2, right: 2)
+        return UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
     }
     
     //セルサイズ
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        let cellWidth = (self.collectionView.bounds.width / 5) - (4 / 5)
-        let cellHeight = (self.collectionView.bounds.height / 6) - (4 / 6)
+        let cellWidth = (self.collectionView.bounds.width / 6) - (1.4 * (6 - 1))
+        let cellHeight = (self.collectionView.bounds.height / 6) - (1 * (5 - 1))
         return CGSize(width: cellWidth, height: cellHeight)
     }
     
@@ -68,7 +73,7 @@ extension PlayGameViewController: UICollectionViewDelegateFlowLayout {
     //
     //　　　　　　□□□）
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
-        return 0
+        return 10
     }
     
     //行間の余白（□ ＜ー＞　□）？？
