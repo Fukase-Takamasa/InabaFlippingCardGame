9a72b09f1f9412ca0b7d851dd235298a5c945556
Takamasa Fukase <takama224@yahoo.co.jp>
2020-03-11 22:06:35 +0900

    変数名の変更

--- InabaFlippingCardGame/PlayGameViewController.swift
+++ InabaFlippingCardGame/PlayGameViewController.swift
@@ -23,16 +23,13 @@ class PlayGameViewController: UIViewController, StoryboardInstantiatable {
     }
     
     var defaultStore: Firestore!
-
-    @IBOutlet weak var collectionView: UICollectionView!
-    
-    //（画像名、isOpenedのBool値、isMatchedのBool値）
-//    var inabaCards: [(UIImage, Bool, Bool)] = []
-    var newInabaCards: [CardData] = []
+    var inabaCards: [CardData] = []
     var dataBaseListener: ListenerRegistration?
     var flipCount = 1
     var flippedCard = [0, 0]
     var isUserTouchEnabled = true
+
+    @IBOutlet weak var collectionView: UICollectionView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -41,78 +38,40 @@ class PlayGameViewController: UIViewController, StoryboardInstantiatable {
         
         self.dataBaseListener = Firestore.firestore().collection("currentGameTableData").addSnapshotListener({ (snapShot, error) in
             if let snapShot = snapShot {
-                self.newInabaCards = snapShot.documents.map{ data -> CardData in
+                self.inabaCards = snapShot.documents.map{ data -> CardData in
                     let data = data.data()
                     return CardData(imageName: data["imageName"] as! String, isOpened: data["isOpened"] as! Bool, isMatched: data["isMatched"] as! Bool)
                 }
                 self.collectionView.reloadData()
             }
         })
-        
-//        setInabaCard()
-//        inabaCards.shuffle()
-
     }
-    
-//    func setInabaCard() {
-//        inabaCards = [
-//            (UIImage(named: "ina1")!, false, false),
-//            (UIImage(named: "ina2")!, false, false),
-//            (UIImage(named: "ina3")!, false, false),
-//            (UIImage(named: "ina4")!, false, false),
-//            (UIImage(named: "ina5")!, false, false),
-//            (UIImage(named: "ina6")!, false, false),
-//            (UIImage(named: "ina7")!, false, false),
-//            (UIImage(named: "ina8")!, false, false),
-//            (UIImage(named: "ina9")!, false, false),
-//            (UIImage(named: "ina10")!, false, false),
-//            (UIImage(named: "ina11")!, false, false),
-//            (UIImage(named: "ina12")!, false, false),
-//            (UIImage(named: "ina13")!, false, false),
-//            (UIImage(named: "ina14")!, false, false),
-//            (UIImage(named: "ina15")!, false, false),
-//            (UIImage(named: "ina1")!, false, false),
-//            (UIImage(named: "ina2")!, false, false),
-//            (UIImage(named: "ina3")!, false, false),
-//            (UIImage(named: "ina4")!, false, false),
-//            (UIImage(named: "ina5")!, false, false),
-//            (UIImage(named: "ina6")!, false, false),
-//            (UIImage(named: "ina7")!, false, false),
-//            (UIImage(named: "ina8")!, false, false),
-//            (UIImage(named: "ina9")!, false, false),
-//            (UIImage(named: "ina10")!, false, false),
-//            (UIImage(named: "ina11")!, false, false),
-//            (UIImage(named: "ina12")!, false, false),
-//            (UIImage(named: "ina13")!, false, false),
-//            (UIImage(named: "ina14")!, false, false),
-//            (UIImage(named: "ina15")!, false, false)
-//        ]
-//    }
+
 }
 
 extension PlayGameViewController: UICollectionViewDelegate, UICollectionViewDataSource {
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return newInabaCards.count
+        return inabaCards.count
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = CollectionViewUtil.createCell(collectionView, identifier: CardCell.reusableIdentifier, indexPath) as! CardCell
 //        if inabaCards[indexPath.row].2 {
-        if newInabaCards[indexPath.row].isMatched {
+        if inabaCards[indexPath.row].isMatched {
             print("生成時: isMatchedがtrue")
-            cell.imageView.image = UIImage(named: newInabaCards[indexPath.row].imageName)!
-            newInabaCards[indexPath.row].isOpened = true
+            cell.imageView.image = UIImage(named: inabaCards[indexPath.row].imageName)!
+            inabaCards[indexPath.row].isOpened = true
         }else {
             print("生成時: isMatchedがfalse")
             if indexPath.row % 2 == 0 {
-                if self.newInabaCards[indexPath.row].isOpened {
-                    cell.imageView.image = UIImage(named: newInabaCards[indexPath.row].imageName)!
+                if self.inabaCards[indexPath.row].isOpened {
+                    cell.imageView.image = UIImage(named: inabaCards[indexPath.row].imageName)!
                 }else {
                     cell.imageView.image = UIImage(named: "CardBackImageRed")
                 }
             }else {
-                if self.newInabaCards[indexPath.row].isOpened {
-                    cell.imageView.image = UIImage(named: newInabaCards[indexPath.row].imageName)!
+                if self.inabaCards[indexPath.row].isOpened {
+                    cell.imageView.image = UIImage(named: inabaCards[indexPath.row].imageName)!
                 }else {
                     cell.imageView.image = UIImage(named: "CardBackImageBlue")
                 }
@@ -122,29 +81,29 @@ extension PlayGameViewController: UICollectionViewDelegate, UICollectionViewData
     }
     
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
-        if newInabaCards[indexPath.row].isOpened == false {
-            self.newInabaCards[indexPath.row].isOpened = true
+        if inabaCards[indexPath.row].isOpened == false {
+            self.inabaCards[indexPath.row].isOpened = true
             if self.flipCount == 2 {
                 self.flippedCard[1] = indexPath.row
                 //フリップ２回目　２枚がマッチしてるかジャッジ
-                if (newInabaCards[flippedCard[0]].imageName) == (newInabaCards[flippedCard[1]].imageName) {
+                if (inabaCards[flippedCard[0]].imageName) == (inabaCards[flippedCard[1]].imageName) {
                     print("マッチした！")
-                    print("マッチ結果: \(newInabaCards[flippedCard[0]]), \(newInabaCards[flippedCard[1]])")
+                    print("マッチ結果: \(inabaCards[flippedCard[0]]), \(inabaCards[flippedCard[1]])")
                     print("flippedCard: \(flippedCard)")
                     //マッチした！両方のisOpenedをtrueにする
-                    newInabaCards[flippedCard[0]].isMatched = true
-                    newInabaCards[flippedCard[1]].isMatched = true
+                    inabaCards[flippedCard[0]].isMatched = true
+                    inabaCards[flippedCard[1]].isMatched = true
                     self.flipCount = 1
                     self.flippedCard = [0,0]
                 }else {
                     print("マッチしませんでした")
-                    print("マッチ結果: \(newInabaCards[flippedCard[1]]), \(newInabaCards[flippedCard[1]])")
+                    print("マッチ結果: \(inabaCards[flippedCard[1]]), \(inabaCards[flippedCard[1]])")
                     print("flippedCard: \(flippedCard)")
                     collectionView.isUserInteractionEnabled = false
                     DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + 1.5) {
                         //マッチしてないので、両方閉じる
-                        self.newInabaCards[self.flippedCard[0]].isOpened = false
-                        self.newInabaCards[self.flippedCard[1]].isOpened = false
+                        self.inabaCards[self.flippedCard[0]].isOpened = false
+                        self.inabaCards[self.flippedCard[1]].isOpened = false
                         self.flipCount = 1
                         self.flippedCard = [0,0]
                         collectionView.isUserInteractionEnabled = true
