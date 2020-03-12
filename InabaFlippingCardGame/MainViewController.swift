2daf23963c164b53539bfa7cd3535cf62fd07179
深瀬 貴将 <fukase@arsaga.jp>
2020-03-12 19:52:24 +0900

    トップページでDBを更新するボタン設置。ゲーム画面に入る前にここでカードデータを設定

--- InabaFlippingCardGame/MainViewController.swift
+++ InabaFlippingCardGame/MainViewController.swift
@@ -12,16 +12,19 @@ import RxCocoa
 import Instantiate
 import InstantiateStandard
 import PKHUD
+import Firebase
 
 class MainViewController: UIViewController, StoryboardInstantiatable {
     
     let dispopseBag = DisposeBag()
+    var db: Firestore!
 
     @IBOutlet weak var textField: UITextField!
     @IBOutlet weak var startButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        db = Firestore.firestore()
         
         //other
         startButton.rx.tap.subscribe{ _ in
@@ -33,7 +36,67 @@ class MainViewController: UIViewController, StoryboardInstantiatable {
             
         }.disposed(by: dispopseBag)
     }
-
+    
+    @IBAction func setData(_ sender: Any) {
+        for i in 1...15 {
+            db.collection("currentGameTableData").document("cardData\(i)").setData([
+                "imageName": "ina\(i)",
+                "isOpened": false,
+                "isMatched": false,
+                "id": i
+            ], merge: true) { err in
+                print(i)
+                if let err = err {
+                    print("errです: \(err)")
+                }else {
+                    print("setData Succesful (\(i))")
+                }
+            }
+            db.collection("currentGameTableData").document("cardData\(i + 15)").setData([
+                "imageName": "ina\(i)",
+                "isOpened": false,
+                "isMatched": false,
+                "id": (i + 15)
+            ], merge: true) { err in
+                print(i + 15)
+                if let err = err {
+                    print("errです: \(err)")
+                }else {
+                    print("setData Succesful(\(i + 15))")
+                }
+            }
+        }
+        
+        
+//        for i in 1...15 {
+//                db.collection("currentGameTableData").document("cardData\(i)").setData([
+////                    "imageName": "ina\(i)"
+//                    "isOpened": false,
+//                    "isMatched": false
+//                ], merge: true) { err in
+//                    print(i)
+//                    if let err = err {
+//                        print("errです: \(err)")
+//                    }else {
+//                        print("setData Succesful")
+//                    }
+//                }
+//                db.collection("currentGameTableData").document("cardData\(i + 15)").setData([
+////                    "imageName": "ina\(i)"
+//                    "isOpened": false,
+//                    "isMatched": false
+//                ], merge: true) { err in
+//                    print(i)
+//                    if let err = err {
+//                        print("errです: \(err)")
+//                    }else {
+//                        print("setData Succesful(+15)")
+//                    }
+//                }
+//        }
+        
+    }
+    
 
 }
 
