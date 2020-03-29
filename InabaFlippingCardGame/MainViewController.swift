471de298488b5e392f34add05cc2115abce716e3
深瀬 貴将 <fukase@arsaga.jp>
2020-03-29 16:14:44 +0900

    DBリセットボタン　コード削除。Xボタン削除

--- InabaFlippingCardGame/MainViewController.swift
+++ InabaFlippingCardGame/MainViewController.swift
@@ -42,46 +42,6 @@ class MainViewController: UIViewController, StoryboardInstantiatable {
             }
         }.disposed(by: dispopseBag)
     }
-
-    @IBAction func setData(_ sender: Any) {
-        HUD.show(.progress)
-        DispatchQueue.main.asyncAfter(deadline: .now() + 5) {
-            HUD.flash(.success, delay: 1.5) { (Bool) in
-                HUD.hide()
-            }
-        }
-        for i in 1...15 {
-            db.collection("currentGameTableData").document("cardData\(i)").setData([
-                "imageName": "ina\(i)",
-                "isOpened": false,
-                "isMatched": false,
-                "id": i
-            ], merge: true) { err in
-                print(i)
-                if let err = err {
-                    print("errです: \(err)")
-                }else {
-                    print("setData Succesful (\(i))")
-                }
-            }
-            db.collection("currentGameTableData").document("cardData\(i + 15)").setData([
-                "imageName": "ina\(i)",
-                "isOpened": false,
-                "isMatched": false,
-                "id": (i + 15)
-            ], merge: true) { err in
-                print(i + 15)
-                if let err = err {
-                    print("errです: \(err)")
-                }else {
-                    print("setData Succesful(\(i + 15))")
-                }
-            }
-            if (i + 15) == 30 {
-                print("loaded completed")
-            }
-        }
-    }
     
     func CompOrErr(err: Error?, i: Int, _ start: Date) {
         if let err = err {
