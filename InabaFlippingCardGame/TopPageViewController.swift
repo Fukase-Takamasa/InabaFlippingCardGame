6324451bb7cf09cca60653c108d3ffb62a8cc680
深瀬 貴将 <fukase@arsaga.jp>
2020-04-04 21:46:54 +0900

    遷移前にプレイ人数を取得し、満室ならアラート、0人ならカードのセット、1人なら名前だけ送信して遷移。エラーもアラート作成。

--- InabaFlippingCardGame/TopPageViewController.swift
+++ InabaFlippingCardGame/TopPageViewController.swift
@@ -18,6 +18,7 @@ class TopPageViewController: UIViewController, StoryboardInstantiatable {
     
     let dispopseBag = DisposeBag()
     var db: Firestore!
+    var start = Date()
     let uuidString = UUID().uuidString
     var thirtyNumbers: [Int] = []
     var myPlayerName = "名無しさん"
@@ -46,6 +47,13 @@ class TopPageViewController: UIViewController, StoryboardInstantiatable {
                     self.myPlayerName = "名無しさん"
                 }
             }).disposed(by: dispopseBag)
+        playerNameTextField.rx.controlEvent(.editingDidEnd).asDriver()
+            .drive(onNext: {
+                if self.playerNameTextField.text == "" {
+                    self.playerNameTextField.text = "ニックネーム未設定"
+                    self.myPlayerName = "名無しさん"
+                }
+            }).disposed(by: dispopseBag)
         
         fightWithYourselfButton.rx.tap.subscribe{ _ in
             let vc = PlayGameViewController.instantiate()
@@ -54,68 +62,124 @@ class TopPageViewController: UIViewController, StoryboardInstantiatable {
         }.disposed(by: dispopseBag)
         
         playWithCpuButton.rx.tap.subscribe{ _ in
-            self.showAlert()
+            self.comingSoonAlert()
         }.disposed(by: dispopseBag)
         
         tableView.rx.itemSelected.subscribe(onNext: { [unowned self] indexPath in
+            HUD.show(.progress)
+            //処理時間を計測するため、tableViewタップ時に処理開始時間を更新
+            self.start = Date()
+            //セルタップするごとに1~30の数字をシャッフル
             print("シャッフル前: \(self.thirtyNumbers)")
-            let start = Date()
+            self.thirtyNumbers.shuffle()
+            print("シャッフル後: \(self.thirtyNumbers)")
             if indexPath.section == 0 {
-                self.showAlert()
+                self.comingSoonAlert()
                 self.tableView.deselectRow(at: indexPath, animated: true)
             }else {
-                //セルタップするごとに1~30の数字をシャッフル
-                self.thirtyNumbers.shuffle()
-                print("シャッフル後: \(self.thirtyNumbers)")
-                HUD.show(.progress)
-                self.db.collection("rooms")
-                    .document("room\(indexPath.row + 1)")
-                    .setData([
-                        "\(self.uuidString)": self.myPlayerName,
-                        "currentFlippingPlayer": "player1"
-                    ], merge: true)
-                for i in 1...30 {
-                    let random = self.thirtyNumbers[i - 1]
-                    self.db.collection("rooms")
-                        .document("room\(indexPath.row + 1)")
-                        .collection("cardData")
-                        .document("cardData\(random)")
-                        .setData([
-                            "imageName": "ina\(i > 15 ? (i - 15) : (i))",  //←3項演算子
-                            "isOpened": false,
-                            "isMatched": false,
-                            "id": random
-                        ], merge: true) { err in
-                            self.CompOrErr(err, i, start, indexPath)
+                self.db.collection("rooms").document("room\(indexPath.row + 1)").getDocument { (docListSnapshot, err) in
+                    guard let docList = docListSnapshot?.data() else {
+                        if let err = err {
+                            print("getDocument Error: \(String(describing: err))")
+                        }else { print("docListが空です") }
+                        self.errorOccuredAlert()
+                        return
                     }
+                    print("docList取得成功 データ数: \(docList.count)")
+                    print("docList中身: \(docList)")
+                    if docList.count > 2 {
+                        print("ルームが満室です。")
+                        self.roomIsFullAlert()
+                    }else {
+                        print("ルームに入室可能です\n接続を開始します。")
+                        self.db.collection("rooms")
+                            .document("room\(indexPath.row + 1)")
+                            .setData([
+                                "\(self.uuidString)": self.myPlayerName,
+                                "currentFlippingPlayer": "player1"
+                            ], merge: true) { err in
+                                if docList.count == 1 {
+                                    self.setCardData(indexPath: indexPath)
+                                }else {
+                                    self.goToGamePage(indexPath: indexPath)
+                                }
+                        }
+                    }
+                    
                 }
             }
         }).disposed(by: dispopseBag)
-
     }
     
-    func CompOrErr(_ err: Error?, _ i: Int, _ start: Date, _ indexPath: IndexPath) {
-        if let err = err {
-            print("index(\(i))errです: \(err)")
-        }else {
-            print("setData Succesful (\(i))")
-            if i == 30 {
-                let elapsedTime = Date().timeIntervalSince(start)
-                print("30 Cards data set completed!")
-                print("処理時間: \(elapsedTime)秒")
-                HUD.flash(.success, delay: 1) { (Bool) in
-                    let vc = PlayGameViewController.instantiate()
-                    vc.roomNumber = (indexPath.row + 1)
-                    vc.myUUID = self.uuidString
-                    vc.gameType = .fireStoreOnline
-                    self.tableView.deselectRow(at: indexPath, animated: true)
-                    self.navigationController?.pushViewController(vc, animated: true)
-                }
+    func setCardData(indexPath: IndexPath) {
+        for i in 1...30 {
+            let random = self.thirtyNumbers[i - 1]
+            self.db.collection("rooms")
+                .document("room\(indexPath.row + 1)")
+                .collection("cardData")
+                .document("cardData\(random)")
+                .setData([
+                    "imageName": "ina\(i > 15 ? (i - 15) : (i))",  //←3項演算子
+                    "isOpened": false,
+                    "isMatched": false,
+                    "id": random,
+                    "correctedPlayer": ""
+                    //後で　誰がマッチさせたか　のフィールドを追加する
+                ], merge: true) { err in
+                    if let err = err {
+                        print("setCardData Error: \(String(describing: err))")
+                    }else {
+                        self.setCardsCompOrErr(i, indexPath, err)
+                    }
             }
         }
     }
     
-    func showAlert() {
+    func setCardsCompOrErr( _ i: Int, _ indexPath: IndexPath, _ err: Error?) {
+        if let err = err {
+            print("index(\(i)) setCardDataErrです: \(err)")
+        }else {
+            print("setData Succesful (\(i))")
+            if i == 30 {
+                print("30 Cards data set completed!")
+                self.goToGamePage(indexPath: indexPath)
+            }
+        }
+    }
+    
+    func goToGamePage(indexPath: IndexPath) {
+        HUD.flash(.success, delay: 1) { (Bool) in
+            let vc = PlayGameViewController.instantiate()
+            vc.roomNumber = (indexPath.row + 1)
+            vc.myUUID = self.uuidString
+            vc.gameType = .fireStoreOnline
+            self.tableView.deselectRow(at: indexPath, animated: true)
+            print("処理時間: \(Date().timeIntervalSince(self.start - 1))秒")
+            self.navigationController?.pushViewController(vc, animated: true)
+        }
+    }
+    
+    func errorOccuredAlert() {
+        HUD.hide()
+        //後で余裕があれば、他のルームに自動で接続し直しますか？を作る　それでも他の部屋が満室なら自動で新しい部屋を作成し、新しい部屋を作成しました　のアラートを出す
+        let alert = UIAlertController(title: "接続に失敗しました", message: "通信状況を確認するか、時間を空けてまたお試しください", preferredStyle: .alert)
+        let ok = UIAlertAction(title: "OK", style: .default) { (UIAlertAction) in
+        }
+        alert.addAction(ok)
+        self.present(alert, animated: true, completion: nil)
+    }
+    
+    func roomIsFullAlert() {
+        HUD.hide()
+        let alert = UIAlertController(title: "このルームは満室です", message: "ルームを変更するか「＋今すぐ作成」\nをお試しください", preferredStyle: .alert)
+        let ok = UIAlertAction(title: "OK", style: .default) { (UIAlertAction) in
+        }
+        alert.addAction(ok)
+        self.present(alert, animated: true, completion: nil)
+    }
+    
+    func comingSoonAlert() {
+        HUD.hide()
         let alert = UIAlertController(title: "この機能は準備中です", message: "乞うご期待!", preferredStyle: .alert)
         let ok = UIAlertAction(title: "OK", style: .default) { (UIAlertAction) in
         }
@@ -142,7 +206,7 @@ extension TopPageViewController: UITableViewDelegate, UITableViewDataSource {
         if section == 0 {
             return 1
         }else {
-            return 10
+            return 3
         }
     }
     
@@ -158,12 +222,12 @@ extension TopPageViewController: UITableViewDelegate, UITableViewDataSource {
             return cell
         }else {
             switch indexPath.row {
-            case 9:
-                cell.roomNameLabel.text = "ルーム1\(indexPath.row + 1)　　1/2人"
-                cell.roomStateLabel.text = "参加する"
-                cell.roomStateLabelBaseView.isHidden = false
+            case 1:
+                cell.roomNameLabel.text = "ルーム\(indexPath.row + 1)　　2/2人"
+                cell.roomStateLabel.text = "満室"
+                cell.roomStateLabelBaseView.backgroundColor = UIColor.systemOrange
             default:
-                cell.roomNameLabel.text = "ルーム10\(indexPath.row + 1)　　0/2人"
+                cell.roomNameLabel.text = "ルーム\(indexPath.row + 1)　　0/2人"
                 cell.roomStateLabel.text = "参加する"
                 cell.roomStateLabelBaseView.backgroundColor = UIColor.systemTeal
             }
