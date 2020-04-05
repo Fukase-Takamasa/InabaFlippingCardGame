2d7f006e4f4e849e7b0c6deb5f43e375a6f87589
深瀬 貴将 <fukase@arsaga.jp>
2020-04-05 14:39:15 +0900

    ①1~30の"ランダム順" と "正順"の同時生成を1行に。　②アラート3種あったのをenumで1つにまとめ。

--- InabaFlippingCardGame/TopPageViewController.swift
+++ InabaFlippingCardGame/TopPageViewController.swift
@@ -21,13 +21,18 @@ class TopPageViewController: UIViewController, StoryboardInstantiatable {
         var playerCount: Int
     }
     
-    let dispopseBag = DisposeBag()
-    var db: Firestore!
+    enum AlertType {
+        case error
+        case full
+        case comingSoon
+    }
     
+    let dispopseBag = DisposeBag()
+    let uuidString = UUID().uuidString
+    var db: Firestore!
+    var alertType: AlertType?
     var rooms: [Rooms] = []
     var start = Date()
-    let uuidString = UUID().uuidString
-    var thirtyNumbers: [Int] = []
     var myPlayerName = "名無しさん"
 
     @IBOutlet weak var playerNameTextField: UITextField!
@@ -37,41 +42,27 @@ class TopPageViewController: UIViewController, StoryboardInstantiatable {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        for i in 1...30 {
-            thirtyNumbers += [i]
-        }
-        
         TableViewUtil.registerCell(tableView, identifier: TopPageRoomListCell.reusableIdentifier)
 
         db = Firestore.firestore()
-
+        //ロビーに表示するオンラインルーム一覧情報の自動更新を設定
         db.collection("rooms").addSnapshotListener{ snapshot, err in
             guard let snapshot = snapshot else {
-                print("snapshotListener Error: \(String(describing: err))")
-                return
+                print("snapshotListener Error: \(String(describing: err))"); return
             }
             self.rooms = snapshot.documents.map { data -> Rooms in
                 return Rooms(roomName: data.documentID, playerCount: data.data().count - 1)
             }
             self.tableView.reloadData()
-//            HUD.show(.progress)
-//            DispatchQueue.main.asyncAfter(deadline: .now() + 0.15) {
-//                HUD.hide()
-//            }
         }
-        
         
         //Rxメソッド
         tableView.rx.itemSelected.subscribe(onNext: { [unowned self] indexPath in
             HUD.show(.progress)
             //処理時間を計測するため、tableViewタップ時に処理開始時間を更新
             self.start = Date()
-            //セルタップするごとに1~30の数字をシャッフル
-            print("シャッフル前: \(self.thirtyNumbers)")
-            self.thirtyNumbers.shuffle()
-            print("シャッフル後: \(self.thirtyNumbers)")
             if indexPath.section == 0 {
-                self.comingSoonAlert()
+                self.showAlert(type: .comingSoon)
                 self.tableView.deselectRow(at: indexPath, animated: true)
             }else {
                 self.db.collection("rooms").document("room\(indexPath.row + 1)").getDocument { (docListSnapshot, err) in
@@ -79,14 +70,14 @@ class TopPageViewController: UIViewController, StoryboardInstantiatable {
                         if let err = err {
                             print("getDocument Error: \(String(describing: err))")
                         }else { print("docListが空です") }
-                        self.errorOccuredAlert()
+                        self.showAlert(type: .error)
                         return
                     }
                     print("docList取得成功 データ数: \(docList.count)")
                     print("docList中身: \(docList)")
                     if docList.count > 2 {
                         print("ルームが満室です。")
-                        self.roomIsFullAlert()
+                        self.showAlert(type: .full)
                     }else {
                         print("ルームに入室可能です\n接続を開始します。")
                         self.db.collection("rooms")
@@ -130,29 +121,27 @@ class TopPageViewController: UIViewController, StoryboardInstantiatable {
         }.disposed(by: dispopseBag)
         
         playWithCpuButton.rx.tap.subscribe{ _ in
-            self.comingSoonAlert()
+            self.showAlert(type: .comingSoon)
         }.disposed(by: dispopseBag)
     }
     
     func setCardData(indexPath: IndexPath) {
-        for i in 1...30 {
-            let random = self.thirtyNumbers[i - 1]
+        for (i, random) in (1...30).shuffled().enumerated() {
             self.db.collection("rooms")
                 .document("room\(indexPath.row + 1)")
                 .collection("cardData")
-                .document("cardData\(random)")
+                .document("cardData\(i + 1)")
                 .setData([
-                    "imageName": "ina\(i > 15 ? (i - 15) : (i))",  //←3項演算子
+                    "imageName": "ina\(random > 15 ? (random - 15) : (random))",
                     "isOpened": false,
                     "isMatched": false,
-                    "id": random,
+                    "id": i + 1,
                     "correctedPlayer": ""
-                    //後で　誰がマッチさせたか　のフィールドを追加する
                 ], merge: true) { err in
                     if let err = err {
                         print("setCardData Error: \(String(describing: err))")
                     }else {
-                        self.setCardsCompOrErr(i, indexPath, err)
+                        self.setCardsCompOrErr(i + 1, indexPath, err)
                     }
             }
         }
@@ -182,33 +171,25 @@ class TopPageViewController: UIViewController, StoryboardInstantiatable {
         }
     }
     
-    func errorOccuredAlert() {
+    func showAlert(type: AlertType?) {
         HUD.hide()
-        //後で余裕があれば、他のルームに自動で接続し直しますか？を作る　それでも他の部屋が満室なら自動で新しい部屋を作成し、新しい部屋を作成しました　のアラートを出す
-        let alert = UIAlertController(title: "接続に失敗しました", message: "通信状況を確認するか、時間を空けてまたお試しください", preferredStyle: .alert)
-        let ok = UIAlertAction(title: "OK", style: .default) { (UIAlertAction) in
+        var alert: UIAlertController!
+        switch alertType {
+            //後で余裕があれば、他のルームに自動で接続し直しますか？を作る　それでも他の部屋が満室なら自動で新しい部屋を作成し、新しい部屋を作成しました　のアラートを出す
+        case .full:
+            alert = UIAlertController(title: "このルームは満室です", message: "ルームを変更するか「＋今すぐ作成」\nをお試しください", preferredStyle: .alert)
+        case .error:
+            alert = UIAlertController(title: "接続に失敗しました", message: "通信状況を確認するか、時間を空けてまたお試しください", preferredStyle: .alert)
+        case .comingSoon:
+            alert = UIAlertController(title: "この機能は準備中です", message: "乞うご期待!", preferredStyle: .alert)
+        case .none:
+            break
         }
+        let ok = UIAlertAction(title: "OK", style: .default) { (UIAlertAction) in }
         alert.addAction(ok)
         self.present(alert, animated: true, completion: nil)
     }
     
-    func roomIsFullAlert() {
-        HUD.hide()
-        let alert = UIAlertController(title: "このルームは満室です", message: "ルームを変更するか「＋今すぐ作成」\nをお試しください", preferredStyle: .alert)
-        let ok = UIAlertAction(title: "OK", style: .default) { (UIAlertAction) in
-        }
-        alert.addAction(ok)
-        self.present(alert, animated: true, completion: nil)
-    }
-    
-    func comingSoonAlert() {
-        HUD.hide()
-        let alert = UIAlertController(title: "この機能は準備中です", message: "乞うご期待!", preferredStyle: .alert)
-        let ok = UIAlertAction(title: "OK", style: .default) { (UIAlertAction) in
-        }
-        alert.addAction(ok)
-        self.present(alert, animated: true, completion: nil)
-    }
 }
 
 extension TopPageViewController: UITableViewDelegate, UITableViewDataSource {
@@ -268,7 +249,7 @@ extension TopPageViewController: UITableViewDelegate, UITableViewDataSource {
 
 extension TopPageViewController: UITextFieldDelegate {
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
-        resignFirstResponder()
+        textField.resignFirstResponder()
         return true
     }
     
