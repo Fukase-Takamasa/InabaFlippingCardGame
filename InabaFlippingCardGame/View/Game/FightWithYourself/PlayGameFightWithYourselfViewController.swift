93be7f02e3ab04d10ad9ebd1a90da09afc5682cb
深瀬 貴将 <fukase@arsaga.jp>
2020-04-09 01:25:15 +0900

    自分との戦い部屋のVC作成　パーツを紐付け

---
+++ InabaFlippingCardGame/View/Game/FightWithYourself/PlayGameFightWithYourselfViewController.swift
@@ -0,0 +1,26 @@
+//
+//  PlayGameFightWithYourselfViewController.swift
+//  InabaFlippingCardGame
+//
+//  Created by 深瀬 貴将 on 2020/04/09.
+//  Copyright © 2020 fukase. All rights reserved.
+//
+
+import UIKit
+
+class PlayGameFightWithYourselfViewController: UIViewController {
+
+    
+    
+    @IBOutlet weak var backButton: UIButton!
+    @IBOutlet weak var turnCountLabel: UILabel!
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+
+
+}
